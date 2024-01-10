b14b73b02a4ad8a12e23237eba110b0203b24063
cyboy <cyril.lewis@hotmail.com>
2024-01-10 00:41:03 +0000

    added framework to package

---
+++ Sources/SlidatechFramework.xcframework/ios-arm64_x86_64-maccatalyst/SlidatechFramework.framework/Versions/A/Headers/SlidatechFramework.h
@@ -0,0 +1,18 @@
+//
+//  SlidatechFramework.h
+//  SlidatechFramework
+//
+//  Created by Cyril Lewis on 09/01/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SlidatechFramework.
+FOUNDATION_EXPORT double SlidatechFrameworkVersionNumber;
+
+//! Project version string for SlidatechFramework.
+FOUNDATION_EXPORT const unsigned char SlidatechFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SlidatechFramework/PublicHeader.h>
+
+
