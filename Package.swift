189feb20333a2fe2eadab388fa43ae338dc6f8f8
cyboy <cyril.lewis@hotmail.com>
2024-01-10 00:41:34 +0000

    checking commit

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "SlidatechFramework",
+    platforms: [
+        .macOS(.v12), .iOS(.v15)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -12,12 +15,10 @@ let package = Package(
             targets: ["SlidatechFramework"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "SlidatechFramework"),
-        .testTarget(
-            name: "SlidatechFrameworkTests",
-            dependencies: ["SlidatechFramework"]),
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .binaryTarget(
+            name: "SlidatechFramework",
+            path: "./Sources/SlidatechFramework.xcframework")
     ]
 )
